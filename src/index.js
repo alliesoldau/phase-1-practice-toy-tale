6f1627efd6c038903de2ac725345fff8c3549ba4
Allie Soldau <alliesoldau@gmail.com>
2022-11-06 06:41:12 -0500

    finished the like button. now just need the fetch andys toys part

--- src/index.js
+++ src/index.js
@@ -15,29 +15,27 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Fetch Andy's Toys
-fetch('http://localhost:3000/toys')
-.then(response => response.json())
-.then(toys => {
-  for (let card in toys){
-    renderToys(toys[card])
-  }
-})
+
 
 // Add Toy Info
 function renderToys(toy) {
   let toyName = document.createElement('h2')
   toyName.innerText = toy.name
+  
   let image = document.createElement('img')
   image.className = "toy-avatar"
   image.src = toy.image
+  
   let likes = document.createElement('p')
   likes.innerText = toy.likes + " likes"
   likes.id = "likesId"
+  
   let button = document.createElement('button')
   button.className = "like-btn"
   button.id = toy.id
   button.innerText = "Like Button <3"
-  button.addEventListener('click', (e) =>updateLikeNumber(e))
+  button.addEventListener('click', (e) => updateLikeNumber(e))
+
   let toyCards = document.getElementById("toy-collection")
   let card = document.createElement('div')
   card.className = "card"
@@ -48,25 +46,43 @@ function renderToys(toy) {
   toyCards.append(card)
 }
 
+// Add a New Toy
+let createToyButton = document.getElementsByClassName('submit')
+createToyButton.addEventListener('submit', (e) => addNewToyCard(e))
+function addNewToyCard(e) {
+console.log(16) }
+
+
+// let newToyButton = document.querySelector('#new-toy-btn')
+// console.log(newToyButton)
+// newToyButton.addEventListener("click", () => addNewToyCard())
+// function addNewToyCard() {
+//   console.log(16)
+// }
+
+
+
+
 // Increase a Toy's Likes
 function updateLikeNumber(e) {
   e.preventDefault()
   let buttonId = e.target.id
+  let newLikes = parseInt(e.target.previousElementSibling.innerText.split(' ')[0], 10) + 1 
+  console.log('new likes: ', newLikes)
   fetch(`http://localhost:3000/toys/${buttonId}`, { 
     method: "PATCH",
     headers: { 
       "Content-Type": "application/json",
       "Accept": "application/json", 
-    }
-  })
-    .then(response => response.json())
-    .then(object => {
-      let likeAmount = object.likes
-      let incrementAmount = likeAmount++
-      console.log(likeAmount)
+    },
+    // the body is how we send info TO the server
+    body: JSON.stringify({ // the info we actually send to the server 
+      "likes": newLikes
+    }) 
     })
-}
-
-  
+    .then(response => response.json())
+    .then(response => e.target.previousElementSibling.innerText = `${newLikes} likes`)
+    }
+ 
   
 
